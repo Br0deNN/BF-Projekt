7871481492b4304ec2e485b1a36953d371a85059
7R1XY <theo.broden@gmail.com>
2025-03-18 10:08:27 +0100

    uppladning i guess

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 
 public class Questions : MonoBehaviour
 {
-    [SerializeField] List<GameObject> questions = new List<GameObject>();
+    [SerializeField] List<GameObject> activeQuestions = new List<GameObject>();
+    List<GameObject> usedQuestions = new List<GameObject>();
 
     public void ShowQuestions()
     {
+        int r = Random.Range(0, activeQuestions.Count);
+        activeQuestions[r].SetActive(true);
+
+        usedQuestions.Add(activeQuestions[r]);
+        activeQuestions.RemoveAt(r);
+        if(activeQuestions.Count == 0 )
+        {
+            activeQuestions = usedQuestions;
+            usedQuestions = new List<GameObject>(usedQuestions);
+            
+        }
 
     }
+
+
+
 }
