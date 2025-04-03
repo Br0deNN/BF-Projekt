d724fa235940e1b0869672027b62e6d5bd71a6ac
baronvonpurr <robinhood.svensson@hotmail.com>
2025-04-03 13:24:35 +0200

    Fixxaaa

--- Assets/ClickTest.cs
+++ Assets/ClickTest.cs
@@ -6,6 +6,7 @@ public class Click2DObject : MonoBehaviour
     public GameObject button;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 >>>>>>> Stashed changes
     void Update()
@@ -17,6 +18,13 @@ public class Click2DObject : MonoBehaviour
             button.SetActive(true);
             this.gameObject.SetActive(false);
         }
+=======
+    public void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        button.SetActive(true);
+        this.gameObject.SetActive(false);
+>>>>>>> Stashed changes
 =======
     public void OnMouseDown()
     {
