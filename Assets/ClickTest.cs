0811e7ea456151d909b2b65bc4c888d8826dfc8f
baronvonpurr <robinhood.svensson@hotmail.com>
2025-04-03 13:27:19 +0200

    Revert "Updatera"
    This reverts commit 4abff7c8a70215a6174573862817b307dc6d39fd.

--- Assets/ClickTest.cs
+++ Assets/ClickTest.cs
@@ -5,7 +5,6 @@ public class Click2DObject : MonoBehaviour
 <<<<<<< Updated upstream
     public GameObject button;
 
-<<<<<<< Updated upstream
 =======
 >>>>>>> Stashed changes
     void Update()
@@ -17,13 +16,6 @@ public class Click2DObject : MonoBehaviour
             button.SetActive(true);
             this.gameObject.SetActive(false);
         }
-=======
-    public void OnMouseDown()
-    {
-        Debug.Log("Clicked");
-        button.SetActive(true);
-        this.gameObject.SetActive(false);
->>>>>>> Stashed changes
     }
 
 =======
