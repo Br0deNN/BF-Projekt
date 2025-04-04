48e4828671a4b90f2fafb2f0f50ec8033adb645b
7R1XY <theo.broden@gmail.com>
2025-04-04 13:57:34 +0200

    bara nallen kvar

--- Assets/Scripts/TeddyBear.cs
+++ Assets/Scripts/TeddyBear.cs
@@ -8,12 +8,10 @@ public class TeddyBear : MonoBehaviour
     public ManagerScript script;
 
 
-    int index = 0;
-
     private void Start()
     {
          
-        GetComponent<Image>().sprite = clothes[index];
+        GetComponent<Image>().sprite = clothes[0];
     }
 
   
@@ -21,9 +19,25 @@ public class TeddyBear : MonoBehaviour
     
     public void ImageSwapper()
     {
-        
-        
-        GetComponent<Image>().sprite = clothes[++index];
+        if(script.starAmount == 1)
+        {
+            GetComponent<Image>().sprite = clothes[1];
+        }
+        else if (script.starAmount == 2)
+        {
+            GetComponent<Image>().sprite = clothes[2];
+        }
+        else if (script.starAmount == 4)
+        {
+            GetComponent<Image>().sprite = clothes[3];
+        }
+        else if (script.starAmount == 5)
+        {
+            transform.position = new Vector3(2.2f , -0.41f , 10);
+            transform.rotation = Quaternion.EulerAngles(new Vector3(52.7f , 0 , 70 ));
+        }
+
+
 
 
 
